58b0b36ce4738a53bfafdccfb983c669a1acbbe3
shailjaatkotiya <shailjaatkotiya@promactinfo.com>
2024-03-14 15:08:39 +0530

    Updated Project

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using EmployeeManagement;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement;
+
+public partial class Employee
+{
+    public string? Name { get; set; }
+
+    public string? Department { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+
+    public Guid Id { get; set; }
+
+    public long? Experience { get; set; }
+}
---
+++ EmployeeManagementContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement;
+
+public partial class EmployeeManagementContext : DbContext
+{
+    public EmployeeManagementContext()
+    {
+    }
+
+    public EmployeeManagementContext(DbContextOptions<EmployeeManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=EmployeeManagement;Username=postgres;Password=shailja21");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Employee_pkey");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
